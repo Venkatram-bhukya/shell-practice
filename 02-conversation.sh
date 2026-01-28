37d387ea43e51ea1bf4c8cf4fa3ce221dcb0f4ce
bhukyadevops@gmail.com <bhukyadevops@gmail.com>
2026-01-28 21:59:55 +0530

    action

---
+++ 02-conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "suresh:: Hello ramesh, How are you?"
+echo "ramesh:: Hi suresh, I am fine thanks for asking, How are you doing?"
+echo "suresh:: doing fine, what are you lerning ramesh?"
+echo "ramesh:: I am learning shell, what about you?"
+
+
