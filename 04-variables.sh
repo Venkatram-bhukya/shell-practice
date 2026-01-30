1d2742395c7a4af50293808e7b30f63801656195
bhukyadevops@gmail.com <bhukyadevops@gmail.com>
2026-01-30 14:25:56 +0530

    action

--- 04-variables.sh
+++ 04-variables.sh
@@ -7,3 +7,4 @@ echo "$PERSON1:: Hello $PERSON2,How are you?"
 echo "$PERSON2:: Hi $PERSON1, I am fine thanks for asking, How are you doing?"
 echo "$PERSON1:: doing fine, what are you lerning $PERSON2?"
 echo "$PERSON2:: I am learning shell, what about you?"
+
