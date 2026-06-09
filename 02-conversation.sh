fcec0f2e17aca4e3ec62b5f4287d5adbc6c715e3
bhukyadevops@gmail.com <bhukyadevops@gmail.com>
2026-06-09 10:13:59 +0530

    actions

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -3,7 +3,7 @@
 echo "Vemkatram:: Hello Bindu Priya, How are you?"
 echo "Bindu Priya:: Hi Vemkatram, I am fine thanks for asking, How are you doing?"
 echo "Vemkatram:: doing fine, what are you lerning Bindu Priya?"
-echo "Bindu Priya:: I am learning shell, what about you?"
+echo "Bindu Priya:: I am learning shell, what about you?" 
 
  
 
